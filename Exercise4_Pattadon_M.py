5809dd1bf9b0758ae1cb034c919b922a0bb94fbf
Pattadon7642 <pattadon2003@gmail.com>
2021-05-08 00:12:28 +0700

    Added Score

---
+++ Exercise4_Pattadon_M.py
@@ -0,0 +1,10 @@
+FS = 45
+GB = 50
+IS = 42.5
+CP = 53.5
+
+print("--------YOUR SCORE--------")
+print("Foundation English : ", FS)
+print("General Business :", GB)
+print("Introduction to Computer Systems", IS)
+print("Computer Programming", CP)
